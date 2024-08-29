fa896e8641933607528e1a083be2527d0eea4ffc
platowski <latosbassman@o2.pl>
2024-08-29 10:56:14 +0200

    include pyright

---
+++ backend/src/application/exceptions/video_processing.py
@@ -0,0 +1,2 @@
+class VideoProcessingException(Exception):
+    pass
--- backend/src/application/use_case/store_video.py
+++ backend/src/application/use_case/store_video.py
@@ -1,21 +1,23 @@
 from fastapi import UploadFile
 
+from application.exceptions.video_processing import VideoProcessingException
+
 UPLOADS_DIR = "uploads"
 
 
 class StoreVideoUseCase:
 
-    async def execute(self, file: UploadFile):
+    async def execute(self, uploaded_file: UploadFile) -> str:
         try:
-            with open(self.get_video_path(file.filename), "wb") as f:
-                while contents := file.file.read(1024 * 1024):
+            with open(self.get_video_path(uploaded_file.filename), "wb") as f:
+                while contents := uploaded_file.file.read(1024 * 1024):
                     f.write(contents)
         except Exception:
-            return {"message": "There was an error uploading the file"}
+            raise VideoProcessingException("There was an error uploading the file")
         finally:
-            file.file.close()
-        return self.get_video_path(file.filename)
+            uploaded_file.file.close()
+        return self.get_video_path(uploaded_file.filename)
 
     @staticmethod
-    def get_video_path(filename):
+    def get_video_path(filename) -> str:
         return f"{UPLOADS_DIR}/{filename}"
